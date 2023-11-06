2e2a0b01e7648df5ee10245aeb41e75e70dfd8d8
Laurent Toutain <laurent@touta.in>
2023-11-06 22:07:39 +0100

    Update sid.py
    fix rpc and action

--- pyang/plugins/sid.py
+++ pyang/plugins/sid.py
@@ -687,8 +687,7 @@ class SidFile:
                 self.collect_inner_data_nodes(statement.i_children)
 
             elif statement.keyword == 'rpc':
-                self.merge_item('data',
-                                "/%s:%s" % (self.module_name, statement.arg))
+                self.merge_item('data', self.get_path(statement))
                 for substmt in statement.i_children:
                     if substmt.keyword in self.inrpc_keywords:
                         # RFC 9595, Appendix B require to create SID for all
@@ -725,8 +724,9 @@ class SidFile:
                 self.collect_inner_data_nodes(statement.i_children, prefix)
 
             elif statement.keyword == 'action':
-                self.merge_item('data', self.get_path(statement, prefix))
+               self.merge_item('data', self.get_path(statement, prefix))
                 for substmt in statement.i_children:
+                    self.merge_item('data', "%s/%s" % (self.get_path(statement, prefix), substmt.keyword))
                     if substmt.keyword in self.inrpc_keywords:
                         # RFC 9595, Appendix B require to create SID for all
                         # action input and output schema nodes
