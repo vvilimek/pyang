e1a17c2905164def04c08f0f592aaa5419f1c479
Vojtech Vilimek <vojtech.vilimek@nic.cz>
2026-04-22 23:39:11 +0200

    Try to get rid of flake8 error messages

--- pyang/plugins/jstree.py
+++ pyang/plugins/jstree.py
@@ -264,6 +264,7 @@ def emit_bodystart(modules, fd, ctx):
 """)
 
 def emit_tree(modules, fd, ctx, path):
+#    global levelcnt
     for module in modules:
         bstr = ""
         b = module.search_one('belongs-to')
@@ -360,6 +361,7 @@ def print_children(i_children, module, fd, prefix, path, ctx, level=0):
 
 def print_node(s, module, fd, prefix, path, ctx, level=0):
 
+#    global levelcnt
     fontstarttag = ""
     fontendtag = ""
     status = get_status_str(s)
